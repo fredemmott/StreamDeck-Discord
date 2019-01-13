7a2e157d595448b8b62c75bd2772447d6c4f3421
Fred Emmott <git@fredemmott.com>
2019-01-13 09:05:04 -0800

    Add RPC files from discord-rpc demo
    - Ported to use the same JSON library as the ESDSDK.
    - Not included the high-level client as it's pretty tailored to rich
    presence

---
+++ Sources/Discord/rpc_connection.cpp
@@ -0,0 +1,163 @@
+#include "rpc_connection.h"
+
+#include "../Common/EPLJSONUtils.h"
+#include "../Windows/pch.h"
+#include <atomic>
+
+static const int RpcVersion = 1;
+static RpcConnection Instance;
+
+/*static*/ RpcConnection* RpcConnection::Create(const std::string& applicationId)
+{
+    Instance.connection = BaseConnection::Create();
+	Instance.appId = applicationId;
+    return &Instance;
+}
+
+/*static*/ void RpcConnection::Destroy(RpcConnection*& c)
+{
+    c->Close();
+    BaseConnection::Destroy(c->connection);
+    c = nullptr;
+}
+
+void RpcConnection::Open()
+{
+    if (state == State::Connected) {
+        return;
+    }
+
+    if (state == State::Disconnected && !connection->Open()) {
+        return;
+    }
+
+    if (state == State::SentHandshake) {
+		json message;
+		MessageFrame frame;
+        if (Read(frame)) {
+			message = json::parse(frame.message);
+            auto cmd = EPLJSONUtils::GetStringByName(message, "cmd");
+            auto evt = EPLJSONUtils::GetStringByName(message, "evt");
+            if (cmd == "DISPATCH" && evt == "READY") {
+                state = State::Connected;
+                if (onConnect) {
+                    onConnect(message);
+                }
+            }
+        }
+    }
+    else {
+		json message;
+		message["v"] = RpcVersion;
+		message["client_id"] = appId;
+		const auto json = message.dump();
+        sendFrame.opcode = Opcode::Handshake;
+		sendFrame.length = json.length();
+		strncpy_s(sendFrame.message, json.c_str(), sizeof(sendFrame.message));
+
+        if (this->Write(sendFrame)) {
+            state = State::SentHandshake;
+        }
+        else {
+            Close();
+        }
+    }
+}
+
+bool RpcConnection::Write(const MessageFrame& message) {
+	return connection->Write(&message, sizeof(MessageFrameHeader) + message.length);
+}
+
+void RpcConnection::Close()
+{
+    if (onDisconnect && (state == State::Connected || state == State::SentHandshake)) {
+        onDisconnect(lastErrorCode, lastErrorMessage);
+    }
+    connection->Close();
+    state = State::Disconnected;
+}
+
+void RpcConnection::Write(const json & message)
+{
+	const auto json = message.dump();
+	Write(json.c_str(), json.length());
+}
+
+bool RpcConnection::Write(const void* data, size_t length)
+{
+	dbgprintf("Writing to Discord: %s", data);
+    sendFrame.opcode = Opcode::Frame;
+    memcpy_s(sendFrame.message, sizeof(sendFrame.message), data, length);
+    sendFrame.length = (uint32_t)length;
+    if (!connection->Write(&sendFrame, sizeof(MessageFrameHeader) + length)) {
+        Close();
+        return false;
+    }
+    return true;
+}
+
+bool RpcConnection::Read(json* message) {
+	MessageFrame frame;
+	if (!Read(frame)) {
+		return false;
+	}
+	*message = json::parse(frame.message);
+	return true;
+}
+
+bool RpcConnection::Read(MessageFrame& readFrame)
+{
+    if (state != State::Connected && state != State::SentHandshake) {
+        return false;
+    }
+    for (;;) {
+        bool didRead = connection->Read(&readFrame, sizeof(MessageFrameHeader));
+        if (!didRead) {
+            if (!connection->isOpen) {
+                lastErrorCode = (int)ErrorCode::PipeClosed;
+                lastErrorMessage = "Pipe closed";
+                Close();
+            }
+            return false;
+        }
+
+        if (readFrame.length > 0) {
+            didRead = connection->Read(readFrame.message, readFrame.length);
+            if (!didRead) {
+                lastErrorCode = (int)ErrorCode::ReadCorrupt;
+                lastErrorMessage = "Partial data in frame";
+                Close();
+                return false;
+            }
+            readFrame.message[readFrame.length] = 0;
+        }
+
+        switch (readFrame.opcode) {
+        case Opcode::Close: {
+			auto json = json::parse(readFrame.message);
+            lastErrorCode = EPLJSONUtils::GetIntByName(json, "code");
+            lastErrorMessage = EPLJSONUtils::GetStringByName(json, "message");
+            Close();
+            return false;
+        }
+        case Opcode::Frame:
+			dbgprintf("Reading from discord: %s", readFrame.message);
+            return true;
+        case Opcode::Ping:
+            readFrame.opcode = Opcode::Pong;
+            if (!connection->Write(&readFrame, sizeof(MessageFrameHeader) + readFrame.length)) {
+                Close();
+            }
+            break;
+        case Opcode::Pong:
+            break;
+        case Opcode::Handshake:
+        default:
+            // something bad happened
+            lastErrorCode = (int)ErrorCode::ReadCorrupt;
+			lastErrorMessage = "Bad ipc frame";
+            Close();
+            return false;
+        }
+    }
+}
---
+++ Sources/Discord/rpc_connection.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "connection.h"
+#include "../Common/EPLJSONUtils.h"
+
+#include <string>
+
+// I took this from the buffer size libuv uses for named pipes; I suspect ours would usually be much
+// smaller.
+constexpr size_t MaxRpcFrameSize = 64 * 1024;
+
+struct RpcConnection {
+    enum class ErrorCode : int {
+        Success = 0,
+        PipeClosed = 1,
+        ReadCorrupt = 2,
+    };
+
+    enum class Opcode : uint32_t {
+        Handshake = 0,
+        Frame = 1,
+        Close = 2,
+        Ping = 3,
+        Pong = 4,
+    };
+
+    struct MessageFrameHeader {
+        Opcode opcode;
+        uint32_t length;
+    };
+
+    struct MessageFrame : public MessageFrameHeader {
+        char message[MaxRpcFrameSize - sizeof(MessageFrameHeader)];
+    };
+
+    enum class State : uint32_t {
+        Disconnected,
+        SentHandshake,
+        AwaitingResponse,
+        Connected,
+    };
+
+    BaseConnection* connection{nullptr};
+    State state{State::Disconnected};
+    void (*onConnect)(const json& message){nullptr};
+    void (*onDisconnect)(int errorCode, const std::string& message){nullptr};
+	std::string appId;
+    int lastErrorCode{0};
+	std::string lastErrorMessage;
+    RpcConnection::MessageFrame sendFrame;
+
+    static RpcConnection* Create(const std::string& applicationId);
+    static void Destroy(RpcConnection*&);
+
+    inline bool IsOpen() const { return state == State::Connected; }
+
+    void Open();
+    void Close();
+	void Write(const json& message);
+	bool Read(json* message);
+private:
+    bool Read(MessageFrame& message);
+	bool Write(const MessageFrame& message);
+    bool Write(const void* data, size_t length);
+};
