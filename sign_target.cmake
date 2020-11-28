a983f47eecfc1ccaa5feaeb2cb3cc7263022e834
Fred Emmott <fred@fredemmott.com>
2020-11-28 14:14:49 -0600

    attempt to sign windows executable

---
+++ sign_target.cmake
@@ -0,0 +1,32 @@
+if(WIN32)
+  get_filename_component(
+    WINDOWS_10_KITS_ROOT
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]"
+    ABSOLUTE CACHE
+  )
+  set(WINDOWS_10_KIT_DIR "${WINDOWS_10_KITS_ROOT}/bin/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}" CACHE PATH "Current Windows 10 kit directory")
+  set(SIGNTOOL_KEY_ARGS "" CACHE STRING "Key arguments for signtool.exe - separate with ';'")
+  find_program(
+    SIGNTOOL_EXE
+    signtool
+    PATHS
+    "${WINDOWS_10_KIT_DIR}/x64"
+    "${WINDOWS_10_KIT_DIR}/x86"
+    DOC "Path to signtool.exe if SIGNTOOL_KEY_ARGS is set"
+  )
+endif()
+function(sign_target TARGET)
+  if(SIGNTOOL_KEY_FILE AND WIN32 AND EXISTS("${SIGNTOOL_KEY_FILE}"))
+    add_custom_command(
+      TARGET ${TARGET} POST_BUILD
+      COMMAND
+      "${SIGNTOOL_EXE}"
+      ARGS
+      sign
+      "/f;${SIGNTOOL_KEY_FILE}"
+      /t http://timestamp.digicert.com
+      /fd SHA256
+      "$<TARGET_FILE:${TARGET}>"
+    )
+  endif()
+endfunction()
